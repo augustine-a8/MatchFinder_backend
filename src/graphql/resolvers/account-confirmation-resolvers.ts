5174852b1a3cd196cde39c85c67a2713b39cf348
Augustine Adjalogo-Agbeko <logos.agbeko@gmail.com>
2022-10-22 13:50:27 +0100

    split confirm account resolvers

---
+++ src/graphql/resolvers/account-confirmation-resolvers.ts
@@ -0,0 +1,50 @@
+import { totp } from "notp";
+
+import { User } from "../../model/entities";
+import { signPayload, JWTData, verifyToken } from "../../util/jwt-utils";
+import sendMail from "../../util/nodemailer-config.util";
+import { ContextObj } from "./auth-resolvers";
+
+export default {
+    Mutation: {
+        confirmAccount: async (_: any, { confirmationCode }: { confirmationCode: string }, context: ContextObj) => {
+            const { req, db } = context;
+            const userRepository = db.getRepository(User);
+
+            // get token from request header
+            const authHeader = req?.headers.authorization;
+            const token = authHeader?.split("Bearer ")[1];
+
+            if (!token) {
+                throw new Error("ConfirmAccount: JWT should be provided");
+            }
+
+            const { id, email } = verifyToken(token);
+
+            // find user in db with details obtainede from verifying jwt
+            const user = await userRepository.findOne({ where: { id, email } });
+
+            if (!user) {
+                throw new Error("ConfirmAccount: Incorrect token provided");
+            }
+
+            // verify notp code
+            const notpResult = totp.verify(confirmationCode, token, { time: 60 * 60 * 3 });
+
+            if (!notpResult) {
+                throw new Error("ConfirmAccount: Invalid confirmation code");
+            }
+
+            if (notpResult.delta < 0) {
+                throw new Error("ConfirmAccount: Confirmation code expired, please request new one");
+            }
+
+            // activate account
+            user.accountStatus = "ACTIVE";
+
+            const _user = await user.save();
+
+            return _user;
+        },
+    },
+};
